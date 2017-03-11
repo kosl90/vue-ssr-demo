f523fcb70d6dfa39cbb8bce20e27a13792e1d660
kosl90 <kos1990l@gmail.com>
2017-03-11 15:25:29 +0800

    move codes out of src/

--- src/step-4-complex/app.js
+++
@@ -1,8 +0,0 @@
-const Vue = require('vue');
-
-const Title = require('./title.vue')
-
-export default (context) => {
-  const app = new Vue({template:'<my-title :title="title"></my-title>', components: {MyTitle: Title}, data: {title: 'hh'}});
-  return Promise.resolve(app);
-};
